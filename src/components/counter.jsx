373161312468919ba1982370d0f0ada09cde7f61
Padmini Rai <pmnrai2013@gmail.com>
2021-11-03 09:52:05 +0530

    Dynamic element through state

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,7 +1,13 @@
 import React, { Component } from 'react';
 class Counter extends Component {
+    state = {
+        count : 0
+    }
     render() { 
-        return <h1>Hello World!</h1>;
+            return <div>
+                <span>{this.state.count}</span>
+                <button>inc</button>
+                </div>;
     }
 }
  
--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import 'bootstrap/dist/css/bootstrap.css';
 import Counter from "./components/counter";
 import './index.css';
 import './App.js';
-import App from "./App.js";
 
 ReactDOM.render(
     <React.StrictMode>
