2368fea1373a6fe14b67503d5e860a3bd13bcf2c
Padmini Rai <pmnrai2013@gmail.com>
2021-11-03 10:03:31 +0530

    playing with jsx

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,15 +1,26 @@
 import React, { Component } from 'react';
 class Counter extends Component {
     state = {
-        count : 11
-    }
+        count : 0
+    };
     render() { 
-            return <div>
+            return  (
+                <div>
                 {/* <span>{this.state.count}</span> */}
-                <span>{10+1*3}</span>
+                {/* <span>{10+1*3}</span> */}
+                <span>{this.formatCount()}</span>
                 <button>inc</button>
-                </div>;
+                </div>
+            );
     }
+    formatCount(){
+        const {count} = this.state;
+        // return count===0?'Zero':count;
+        return count===0?<h1>Zero</h1>:count;
+
+    }
+    
 }
- 
+
+
 export default Counter;
