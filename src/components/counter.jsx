3741acc5afa07028654256485695b9011ba588d7
Padmini Rai <pmnrai2013@gmail.com>
2021-11-03 09:54:26 +0530

    UPDATE COUNTER

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 class Counter extends Component {
     state = {
-        count : 0
+        count : 11
     }
     render() { 
             return <div>
-                <span>{this.state.count}</span>
+                {/* <span>{this.state.count}</span> */}
+                <span>{10+1*3}</span>
                 <button>inc</button>
                 </div>;
     }
