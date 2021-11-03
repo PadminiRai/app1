b0da482233267fa6e4463d6e9b46be1f9c7c02df
Padmini Rai <pmnrai2013@gmail.com>
2021-11-03 08:49:39 +0530

    Bug-resolved

--- src/index.js
+++ src/index.js
@@ -2,10 +2,15 @@ import React from "react"
 import ReactDOM from "react-dom"
 import 'bootstrap/dist/css/bootstrap.css';
 import Counter from "./components/counter";
+import './index.css';
+import './App.js';
+import App from "./App.js";
 
 ReactDOM.render(
     <React.StrictMode>
+      {/* <App /> */}
       <Counter />
     </React.StrictMode>,
     document.getElementById("root")
   )
+ 
