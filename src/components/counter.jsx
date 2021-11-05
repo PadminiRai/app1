7685d4715fcc5c737ce34094a82fbcf9211ff4f5
Padmini Rai <pmnrai2013@gmail.com>
2021-11-05 19:00:38 +0530

    Adding image
    With dynamic attribute and property

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 class Counter extends Component {
     state = {
-        count : 0
+        count : 0,
+        imageUrl : 'https://picsum.photos/200'
     };
     render() { 
             return  (
                 <div>
                 {/* <span>{this.state.count}</span> */}
                 {/* <span>{10+1*3}</span> */}
+                <img alt='' src={this.state.imageUrl}/>
                 <span>{this.formatCount()}</span>
                 <button className='btn btn-secondary'>inc</button>
                 </div>
@@ -15,8 +17,9 @@ class Counter extends Component {
     }
     formatCount(){
         const {count} = this.state;
+        const x = <h1>Zero</h1>;
         // return count===0?'Zero':count;
-        return count===0?<h1>Zero</h1>:count;
+        return count===0?x:count;
 
     }
     
