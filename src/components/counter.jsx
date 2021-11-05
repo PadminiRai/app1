cc27ffda11b01b5932aa18dac49dcc12467d14ff
Padmini Rai <pmnrai2013@gmail.com>
2021-11-05 19:21:11 +0530

    JSX based BootStrap Classes

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -11,19 +11,22 @@ style = {
 };
 
     render() { 
+        let classes = 'badge m-3 badge-';
+        classes += this.state.count!==0?'primary':'warning';
+         
             return  (
                 <div>
                 {/* <span>{this.state.count}</span> */}
                 {/* <span>{10+1*3}</span> */}
                 <img alt='' src={this.state.imageUrl}/>
-                <span style={this.style} className='badge badge-primary m-3'>{this.formatCount()}</span>
+                <span style={this.style} className={classes}>{this.formatCount()}</span>
                 <button className='btn btn-secondary btn-sm'>increment</button>
                 </div>
             );
     }
     formatCount(){
         const {count} = this.state;
-        const x = <h1>Zero</h1>;
+        // const x = <h1>Zero</h1>;
         return count===0?'Zero':count;
         // return count===0?x:count;
 
