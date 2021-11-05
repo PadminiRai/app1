513a97c90ae3c24340cf37e73bd3ac2bbb22171c
Padmini Rai <pmnrai2013@gmail.com>
2021-11-05 19:24:36 +0530

    Clean Code

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 class Counter extends Component {
     state = {
-        count : 0,
+        count : 1,
         imageUrl : 'https://picsum.photos/200'
     };
 
@@ -11,26 +11,29 @@ style = {
 };
 
     render() { 
-        let classes = 'badge m-3 badge-';
-        classes += this.state.count!==0?'primary':'warning';
-         
+                
             return  (
                 <div>
                 {/* <span>{this.state.count}</span> */}
                 {/* <span>{10+1*3}</span> */}
                 <img alt='' src={this.state.imageUrl}/>
-                <span style={this.style} className={classes}>{this.formatCount()}</span>
+                <span style={this.style} className={this.getBadgeClass()}>{this.formatCount()}</span>
                 <button className='btn btn-secondary btn-sm'>increment</button>
                 </div>
             );
     }
-    formatCount(){
+getBadgeClass() {
+        let classes = 'badge m-3 badge-';
+        classes += this.state.count !== 0 ? 'primary' : 'warning';
+        return classes
+}
+
+formatCount(){
         const {count} = this.state;
         // const x = <h1>Zero</h1>;
         return count===0?'Zero':count;
         // return count===0?x:count;
-
-    }
+}
     
 }
 
