c6239339167d3988567a15d830d2a14b23b445da
Padmini Rai <pmnrai2013@gmail.com>
2021-11-05 19:09:30 +0530

    Style attribute as JSX object

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -4,22 +4,28 @@ class Counter extends Component {
         count : 0,
         imageUrl : 'https://picsum.photos/200'
     };
+
+style = {
+ fontSize: 75,
+ fontWeight:'bold'
+};
+
     render() { 
             return  (
                 <div>
                 {/* <span>{this.state.count}</span> */}
                 {/* <span>{10+1*3}</span> */}
                 <img alt='' src={this.state.imageUrl}/>
-                <span>{this.formatCount()}</span>
-                <button className='btn btn-secondary'>inc</button>
+                <span style={this.style} className='badge badge-primary m-3'>{this.formatCount()}</span>
+                <button className='btn btn-secondary btn-sm'>increment</button>
                 </div>
             );
     }
     formatCount(){
         const {count} = this.state;
         const x = <h1>Zero</h1>;
-        // return count===0?'Zero':count;
-        return count===0?x:count;
+        return count===0?'Zero':count;
+        // return count===0?x:count;
 
     }
     
