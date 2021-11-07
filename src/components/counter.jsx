22f66724c1011194ae6355f44e64918fb9278202
Padmini Rai <pmnrai2013@gmail.com>
2021-11-07 07:40:25 +0530

    Update
    Constructor , Arrow Function , Binding current object and setting state when value changed.

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -6,11 +6,15 @@ class Counter extends Component {
         tags : ['t1','t2','t3']
 };
 
+// constructor(){
+//     super();
+//     this.incrementCount=this.incrementCount.bind(this);
+// }
+
 style = {
  fontSize: 75,
  fontWeight:'bold'
 };
-
 
 render() { 
                 
@@ -19,8 +23,8 @@ render() {
                 {/* <span>{this.state.count}</span> */}
                 {/* <span>{10+1*3}</span> */}
                 <img alt='' src={this.state.imageUrl}/>
-                <span style={this.style} className={this.getBadgeClass()}>{this.formatCount()}</span>
-                <button onClick='' className='btn btn-secondary btn-sm'>increment</button>
+                <span id='s1' style={this.style} className={this.getBadgeClass()}>{this.formatCount()}</span>
+                <button onClick={this.incrementCount} className='btn btn-secondary btn-sm'>increment</button>
                  <ul>
                      {this.state.tags.map(tags => <li key={tags}>{tags}</li>)}
                  </ul>
@@ -40,7 +44,18 @@ formatCount(){
         return count===0?'Zero':count;
         // return count===0?x:count;
 }
-    
+
+// incrementCount(){
+// console.log('this.state.count+1',this.state.count = this.state.count+1);
+// // this is undefined so either use a constructor to bind 'this' with the function or make it an arrow function.
+
+// }
+  
+incrementCount=()=>{
+    console.log('this.state.count+1', this.state.count+1);
+    this.setState({count:this.state.count+1});
+}
+
 }
 
 
