3d01a34162866d921c4e00131404b933fe6f2e87
Padmini Rai <pmnrai2013@gmail.com>
2021-11-17 18:25:34 +0530

    Update

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 class Counter extends Component {
     state = {
-        count : 0,
-        imageUrl : 'https://picsum.photos/200' ,
-        tags : ['t1','t2','t3']
+        value : this.props.value
+        // ,
+        // imageUrl : 'https://picsum.photos/200' ,
+        // tags : ['t1','t2','t3']
 };
 
 // constructor(){
@@ -17,32 +18,36 @@ style = {
 };
 
 render() { 
-                
+         
+    console.log("props",this.props);
             return  (
                 <div>
+                 <h4> &nbsp;{this.props.id}</h4>
+                {/* {this.props.children} */}
                 {/* <span>{this.state.count}</span> */}
                 {/* <span>{10+1*3}</span> */}
-                <img alt='' src={this.state.imageUrl}/>
+                {/* <img alt='' src={this.state.imageUrl}/> */}
                 <span id='s1' style={this.style} className={this.getBadgeClass()}>{this.formatCount()}</span>
                 {/* onClick is an event handler */}
                 <button onClick={()=>this.incrementCount({id:1})} className='btn btn-secondary btn-sm'>increment</button>
-                 <ul>
+                 {/* <ul>
                      {this.state.tags.map(tags => <li key={tags}>{tags}</li>)}
-                 </ul>
+                 </ul> */}
+                 <button className="btn-sm btn-danger m-2" onClick={() => this.props.onDelete(this.props.id)}>Delete</button>
                 </div>
             );
 }
 
 getBadgeClass() {
         let classes = 'badge m-3 badge-';
-        classes += this.state.count !== 0 ? 'primary' : 'warning';
+        classes += this.state.value !== 0 ? 'primary' : 'warning';
         return classes
 };
 
 formatCount(){
-        const {count} = this.state;
+        const {value} = this.state;
         // const x = <h1>Zero</h1>;
-        return count===0?'Zero':count;
+        return value===0?'Zero':value;
         // return count===0?x:count;
 };
 
@@ -53,8 +58,8 @@ formatCount(){
 // }
   
 incrementCount=product=>{
-    console.log('this.state.count+1', product, this.state.count+1);
-    this.setState({count:this.state.count+1});
+    console.log('this.state.count+1', product, this.state.value+1);
+    this.setState({value:this.state.value+1});
     // when ever changing state vdom gets compared to the DOM and only span gets changed than 
     // the entire page , can be seen in elements while clicking on the button.
 };
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -1,16 +1,28 @@
 import React, { Component } from 'react'
 import Counter from './counter';
 
-class Counters extends React.Component {
+class Counters extends Component {
 
     state={
-        counters :[{id:1,value:0},{id:2,value:0},{id:3,value:0}]
+        counters :[ {id:1,value:5},
+                    {id:2,value:0},
+                    {id:3,value:0} ]
     };
 
+    handleDelete = (counterID) =>{
+
+        console.log("Event called",counterID);
+    }
     render() { 
+      
         return(<div>
            
-                {this.state.counters.map(counters => <Counter key={counters.id}/>)}
+                {this.state.counters.map( counter =>
+                    <Counter key={counter.id} value={counter.value} onDelete={this.handleDelete} id={counter.id} selected={true}>
+                    {/* this is the child either use this or put a id prop in counter tag*/}
+                    {/* <h4>Counter #{counter.id}</h4>   */}
+                    </Counter>  
+                    ) }
            
         </div>) ;
     }
